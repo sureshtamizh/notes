51a8dd14a0244ec9ce6ef3c3e8239a5a38bff83b
Suresh <suresh@datanetiix.com>
2025-02-01 08:27:05 +0530

    ci/cd

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: 'react-native',
+  preset: "react-native",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  moduleNameMapper: {
+    "\\.(png|jpg|jpeg|gif|ttf|woff|woff2|svg)$": "<rootDir>/__mocks__/fileMock.js"
+  }
 };
