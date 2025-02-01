adfb21dce38ba52e5d01ec5d650884b3e3004332
Suresh <suresh@datanetiix.com>
2025-02-01 08:12:43 +0530

    ci/cd

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator, TransitionPresets } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TransitionPresets } from "@react-navigation/stack";
+
 
 
 import Home from "./src/screens/Home";
